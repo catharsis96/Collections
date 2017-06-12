eb506773f0ee3a4808570214617098780f65f46d
catharsis96 <catharsis96@yandex.ru>
2017-06-12 18:02:07 +0300

    generic collections added

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -11,19 +11,22 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            /*
             Car car1 = new Car();
             car1.Make = "Oldsmobile";
             car1.Model = "Cutlas Supreme";
+            car1.VIN = "A1";
 
             Car car2 = new Car();
             car2.Make = "Geo";
             car2.Model = "Prism";
+            car2.VIN = "A2";
 
             Book b1 = new Book();
             b1.Author = "Robert Tabor";
             b1.Title = "Microsoft .NET XML Web Services";
             b1.ISBN = "0-000-00000-0";
-
+            */
             //ArrayList myArrayList = new ArrayList();
             //myArrayList.Add(car1);
             //myArrayList.Add(car2);
@@ -37,13 +40,45 @@ namespace Collections
 
             //Generic List
             //List<T>
+            /*  List<Car> myList = new List<Car>();
+              myList.Add(car1);
+              myList.Add(car2);
+              //we can't to add books to the car's list
+              //but we can:
+              foreach (Car car in myList)
+              {
+                  Console.WriteLine(car.Model);
+              }*/
+
+            /* ************** */
+
+            //Dictionary<TKey, TValue>
+            /*
+            Dictionary<string, Car> myDictionary = new Dictionary<string, Car>();
+
+            myDictionary.Add(car1.VIN, car1);
+            myDictionary.Add(car2.VIN, car2);
+
+            Console.WriteLine(myDictionary["B2"].Make);
+            */
+
+            // string[] names = { "Bob", "Steve", "Brian", "Chuck" };
+            // Object initializer
+            //No need for a Constructor
+            /*
+            Car car1 = new Car() { Make = "BMW", Model = "750li", VIN = "C3"};
+            Car car2 = new Car() { Make = "Toyota", Model = "4Runner", VIN = "D4" };
+            */
             List<Car> myList = new List<Car>();
-            myList.Add(car1);
-            myList.Add(car2);
+            {
+                new Car { Make = "Oldmobile", Model = "Cutlus Supreme", VIN = "E5" };
+                new Car { Make = "Nissan", Model = "Altima", VIN = "F6" };
+            };
         }
     }
     class Car
     {
+        public string VIN { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
 
